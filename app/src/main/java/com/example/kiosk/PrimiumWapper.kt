2a50396364cb0f2619a33203a5f28976a0599ae4
kiwon kim <rlarldnjs17@gmail.com>
2023-12-07 14:18:25 +0900

    키오스크 Lv. 3

--- app/src/main/java/com/example/kiosk/PrimiumWapper.kt
+++ app/src/main/java/com/example/kiosk/PrimiumWapper.kt
@@ -1,7 +1,13 @@
 package com.example.kiosk
 
-class PrimiumWapper (name: String, price: Int) {
+open class PrimiumWapper (name: String, price: Int) {
     fun displayInfo(name: String, price: Int) {
         println ("${name} 주문하였습니다. 이 제품의 가격은 ${price} 원 입니다.")
     }
 }
+
+class QuacheeWapper (name: String, price: Int) : PrimiumWapper(name, price) {
+
+}
+
+class
